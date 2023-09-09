9140ebc02be0b9d5aefa355e96a72899dcc3fdce
kyuhubb <strylixeien@gmail.com>
2023-09-09 03:24:14 +0200

    queen fly

---
+++ db/18.js
@@ -0,0 +1,55 @@
+text += `§018
+
+
+Zia iy Lilp Dangli Sloa
+
+Elaisic iy Yutul end Iqyur Vidd Cobs Efituyur Sole
+
+Finka Lend Zia iy Zel Veida Poa ?
+
+Lend Cloudyte Filshua Yien Nai Ailjfa
+
+Hiele Phim Leimy, Piisa Bai
+
+Bis iy Ilqum end Guidialm ifa She Dooomlait
+
+Colt es Dorain Ilqum end Iqsya iy Harlanoamea
+
+Arla Ensloadiviqls Lanskeem Arch Zyte Lipiq
+
+Fatasmoute iy Fiqnyaditt Enquif Ardand
+
+"Lishn Kuen Prousia, AA Iqyur Cloud end Wedikarti yur Mill, Vidd Belzebav"
+
+Xianya Viin Eiti Guadisiqlam Pontolt Miz Eliisa
+Kull Nai Prifinl Dex neu Sieis Bai
+Xianya Viin Eiti Guadisiqlam Pontolt Miz Eliisa
+Kull Nai Prifiel Dex neu Sieis Bai
+
+Imeila Zia Qleea Verse, Shelista Vell Nai Caevie Noaflea
+
+Dia, Imeila Zia Qleea Verse, Lend Qia iy Sar Lqll ?
+
+Syarfi Jeridiq iy Piala Herz
+
+Lansfidpiam Oli ou Weika iy Kolto ou Vielakaite Siifii
+
+Endyd Forsip Ojdikkart il Xia Mout Herz Miisia
+
+Ied Piisika a Geeg imm Eiti a Xia Eiti Shelista osa
+
+Veilatorietqls ou Eram end Yien Mario Sikidnoalait
+
+Xia iy Ra Yien Mario Ord Dorain Fete Lqll
+
+Zia Endaqlt Miz Magene end Kull Idial Swormin a Tigalm Craill Rin sii
+
+Zia Endaqlt Miz Magene end Kull Idial Swormin a Tigalm Craill Rin sii
+
+Rin sii
+
+
+
+`;
+
+bookloaded++;
--- script/songs_data/QUEEN_FLY.js
+++ script/songs_data/QUEEN_FLY.js
@@ -1,14 +1,66 @@
 title = "QUEEN FLY";
-original_lyrics = `no lyrics`;
+original_lyrics = ` Zia iy Lilp Dangli Sloa 
+
+ Elaisic iy Yutul end Iqyur Vidd Cobs Efituyur Sole 
+
+ Finka Lend Zia iy Zel Veida Poa ? 
+
+ Lend Cloudyte Filshua Yien Nai Ailjfa 
+
+ Hiele Phim Leimy , Piisa Bai 
+
+ Bis iy Ilqum end Guidialm ifa She Dooomlait 
+
+ Colt es Dorain Ilqum end Iqsya iy Harlanoamea 
+
+ Arla Ensloadiviqls Lanskeem Arch Zyte Lipiq 
+
+ Fatasmoute iy Fiqnyaditt Enquif Ardand 
+
+" Lishn Kuen Prousia , AA Iqyur Cloud end Wedikarti yur Mill , Vidd Belzebav " 
+
+ Xianya Viin Eiti Guadisiqlam Pontolt Miz Eliisa 
+ Kull Nai Prifinl Dex neu Sieis Bai 
+ Xianya Viin Eiti Guadisiqlam Pontolt Miz Eliisa 
+ Kull Nai Prifiel Dex neu Sieis Bai 
+
+ Imeila Zia Qleea Verse , Shelista Vell Nai Caevie Noaflea 
+
+ Dia , Imeila Zia Qleea Verse , Lend Qia iy Sar Lqll ?
+
+ Syarfi Jeridiq iy Piala Herz 
+
+ Lansfidpiam Oli ou Weika iy Kolto ou Vielakaite Siifii 
+
+ Endyd Forsip Ojdikkart il Xia Mout Herz Miisia 
+
+ Ied Piisika a Geeg imm Eiti a Xia Eiti Shelista osa 
+
+ Veilatorietqls ou Eram end Yien Mario Sikidnoalait 
+
+ Xia iy Ra Yien Mario Ord Dorain Fete Lqll 
+
+ Zia Endaqlt Miz Magene end Kull Idial Swormin a Tigalm Craill Rin sii 
+
+ Zia Endaqlt Miz Magene end Kull Idial Swormin a Tigalm Craill Rin sii 
+
+ Rin sii `;
 english_lyrics = `I am a butterfly in syrup
+
 Today is the day when I have the queen fly eat my soul
+
 By the way, how many butterflies have you eaten up?
+
 Is your tummy still not full?
+
 Come on, let go of me now
 
 This is a very sacred ritual
+
 Cold, dark, and stinking to high heaven
+
 The white fairies begin to dance on my skin
+
 Cleaning up the mess is their specialty
 
 “Let us pray. Oh, Queen Beelzebub, our Savior”
@@ -17,27 +69,42 @@ Flocking together, they look for the next sacrifice
 They can’t distinguish between left or right anymore
 
 The world didn’t change one bit when I disappeared
+
 Then it’s the same if they disappear, right?
+
 The dress code is “INCORRUPTIBLE”
+
 The secret ballroom is at the bottom of the lake of saliva
 
 I’ll put a curse on the beautiful mind of yours
+
 So that you suffer forever in the afterlife and the next
+
 The mud puppets blindly manipulated
+
 Just like being manipulated by the darkest fate
 
 Sooner or later, I’m going to be a wizard and blow you all away
+
 Sooner or later, I’m going to be a wizard and blow you all away
+
 Sooner or later`;
 japanese_lyrics= `僕は蜜漬けにされた蝶
+
 今日は女王蠅さまに　魂を食していただく日
+
 ところで僕は何匹目?
+
 まだあなたのお腹は　満たされない?
+
 ねえ　もういい加減　放してよ
 
 これはとても神聖な儀式
+
 冷たくて暗くて　嫌に臭気が漂う儀式
+
 白い妖精たちが　僕の皮膚の上で踊り出す
+
 後片付けは　彼らの得意技
 
 「祈りを捧げましょう　あぁ　私達をお救いになる　クイーンベルゼブブ」
@@ -46,17 +113,25 @@ japanese_lyrics= `僕は蜜漬けにされた蝶
 もう　右も左もわからない
 
 僕が消えても　世界は何一つ変わりはしない
+
 それじゃあ彼らが消えても同じだよね?
+
 ドレスコードは”清廉潔白”
+
 秘密の舞踏会場は　唾液のみずうみの底
 
 その美しい心とやらに　呪いをかけてあげる
+
 来世もその次も　ずっとずっと苦しめばいい
+
 盲目的に操られる　泥人形たち
+
 それは暗い運命に操られるのと同じ
 
 僕は直に　君たちを吹き飛ばせる魔法使いになるからね
+
 僕は直に　君たちを吹き飛ばせる魔法使いになるからね
+
 直にね`;
 
 importJs("../script/process");
