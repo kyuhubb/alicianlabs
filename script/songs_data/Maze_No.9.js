86a3b7e9c129c7f9abf150086c1cd7f6a0545d86
kyuhubb <strylixeien@gmail.com>
2023-09-09 22:57:15 +0200

    update Maze No.9

--- script/songs_data/Maze_No.9.js
+++ script/songs_data/Maze_No.9.js
@@ -1,97 +1,143 @@
 title = "Maze No.9";
-original_lyrics = ` Klatsia Esp ou Ticcla, Esp ou Lans
-Hiele, Phier San Eclat Xia
-Minelia a Sail end Saray folme Niar Taz
-Eem Lef Karna Harie Sel Miis
+original_lyrics = ` Klatsia Esp ou Ticcla , Esp ou Lans 
+
+ Hiele , Phier San Eclat Xia 
+
+ Minelia a Sail end Saray folme Niar Taz 
+
+ Eem Lef Karna Harie Sel Miis 
+
+ Olm Sarga , Oulayz Eist , Frait folme Bis 
+
+ Leste , Phier San Xia Fas 
+
+ Hait Aihel imm Bis 
+
+ Baly Crai Ied Fal il Niims Crai Nadi Olis 
+
+ Fiea Zote , Ame Taz Graid
+
+ Mii Emphi Miis Nai Prifiel Ofa Arsia ou Ellif 
+
+ Gout Ene Nai Dilem Olipha , Cate a Geeg end Lili 
 
  
-Olm Sarga, Oulayz Eist, Frait folme Bis
-Leste, Phier San Xia Fas
-Hait Aihel imm Bis
-Baly Crai Ied Fal il Niims Crai Nadi Olis
+ Ijsya ou Nita imm Loqcka Graid , Herz ou Briss a Geeg 
+
+ Ame ou Sar Minelu , Nai Ieha Way 
+ Esp ou Shelista Nai Dilem Arsia 
+
+ Elize imm Krey 
+ Phier Holo Miis imm Aim Taz Madie Ofa Ellif Slay 
+
+ Quim Herz Nai Yien Kelletta Lootaria 
+ Xia Mane Vell Dialoss 
 
  
-Fiea Zote, Ame Taz Graid
-Mii Emphi Miis Nai Prifiel Ofa Arsia ou Ellif
-Gout Ene Nai Dilem Olipha, Cate a Geeg end Lili
+ Fiea Zote , Ame Taz Graid
+
+ Mii Emphi Miis Nai Prifiel Ofa Arsia ou Ellif 
+
+ Gout Ene Nai Dilem Olipha , Cate a Geeg end Lili 
 
  
-Ijsya ou Nita imm Loqcka Graid, Herz ou Briss a Geeg
-Ame ou Sar Minelu, Nai Ieha Way
-Esp ou Shelista Nai Dilem Arsia
-Elize imm Krey
-Phier Holo Miis imm Aim Taz Madie Ofa Ellif Slay
-Quim Herz Nai Yien Kelletta Lootaria
-Xia Mane Vell Dialoss
+ Finz ou Vinelo Mii ol Harmiy Loog , Herz Yiele Awte 
 
- 
-Fiea Zote, Ame Taz Graid
-Mii Emphi Miis Nai Prifiel Ofa Arsia ou Ellif
-Gout Ene Nai Dilem Olipha, Cate a Geeg end Lili
+ Ame ou Sar Minelu , Nai Ieha Way 
+ Esp ou Shelista Nai Dilem Arsia 
 
- 
-Finz ou Vinelo Mii ol Harmiy Loog, Herz Yiele Awte
-Ame ou Sar Minelu, Nai Ieha Way
-Esp ou Shelista Nai Dilem Arsia
-Elize imm Krey
-Phier Holo Miis imm Aim Taz Madie Ofa Ellif Slay
-Quim Herz Nai Yien Amiy Lootaria
-Xia Mane Vell Diallif `;
+ Elize imm Krey 
+ Phier Holo Miis imm Aim Taz Madie Ofa Ellif Slay 
+
+ Quim Herz Nai Yien Amiy Lootaria 
+ Xia Mane Vell Diallif `;
 english_lyrics = `Scattered pieces of glass, dancing pieces
+
 Look, let’s touch their sparkle
+
 Moon and Sun are my friends from childhood
+
 I could remember who one true is
 
 Blue sky, billions of doors, we just start from here
+
 Come on, let’s touch their color
+
 liberty is here
+
 Anytime you can go back to the preterite that you want
 
 Sparkling season, my childhood
+
 I’ve missed my purpose of life
+
 But we don’t need that purpose but just stepping forward
 
 Distant summer scent, hearts that still grow apart
+
 All of them are born from the same star
 We don’t need answer, a piece of the world doesn’t have some reasons
+
 So the least you excuse yourself with smiling when you lose your way
+
 Abandoned heart will die down someday
 
 Sparkling season, my childhood
+
 I’ve missed my purpose of life
+
 But we don’t need that purpose but just stepping forward
 
 End of long winter, hearts that aren’t linked by fate
+
 All of them are born from the same star
 We don’t need answer, a piece of the world doesn’t have some reasons
+
 So the least you excuse yourself with smiling when you lose your way
+
 Loveless heart will die down someday`;
 japanese_lyrics= `散らばったガラスの破片　踊る破片
+
 ほら、その煌めきに触れてみてよ
+
 月と太陽と幼馴染
+
 また　本当の自分を思い出せるような気がする
 
 青空　無数の扉　ここから始まるんだ
+
 さあ、その色彩に触れてみてよ
+
 自由はここに在る
+
 いつだって　帰りたい過去に帰れば良い
 
 眩い季節　幼い頃
+
 一体、何のために生きているのか　判らなくなったことがあった
+
 だけど　人は何のためでもなく　ただ歩いてさえすれば良いんだ
 
 遠い夏の香りも　すれ違ったままの心も
+
 同じ星の子　答えなんて要らない　世界の欠片に理由なんてないよ
+
 だからせめて　迷った時　自分くらいは　笑って赦してあげてね
+
 使われなくなった心は　いつか　枯れてしまうから
 
 眩い季節　幼い頃
+
 一体、何のために生きているのか　判らなくなったことがあった
+
 だけど　人は何のためでもなく　ただ歩いてさえすれば良いんだ
 
 長かった冬の終わりも　出逢うことのなかった心も
+
 同じ星の子　答えなんて要らない　世界の欠片に理由なんてないよ
+
 だからせめて　迷った時　自分くらいは　笑って赦してあげてね
+
 愛されなくなった心は　いつか　死んでしまうから`;
 
 importJs("../script/process");
