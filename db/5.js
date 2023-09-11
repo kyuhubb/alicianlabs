43cf76f8530a080ef1f42cf9340d229957e39ba9
kyuhubb <strylixeien@gmail.com>
2023-09-11 15:07:35 +0200

    added alician for under wonder nights

--- db/5.js
+++ db/5.js
@@ -1,7 +1,42 @@
 text += `§005
 
 
+ Mezart a Floul Veis 
 
+ Oulayz Saut , Verry 
+ Kulu Nai Harie Zel Note 
+
+ Koll Finz ou Kllea , Ranya a Meehellm 
+
+ Xia Kopiita Fimoa ol Verse Imblu 
+
+ Madie Pisca Rustie Naits Eclat Tone 
+
+ Brait Xia Eist ?
+
+ Nouche Erlim end Brait imm Shiita Ween 
+
+ Mill Orfag 
+ Lishe Lans imm Lootaria end Ignai Aihel 
+
+ Kolto o Meele Fiola ou Finz Ween 
+ Mii Nai Dist Crai Foul 
+
+ Leste Yiep Sween 
+
+ Madie Pisca Rustie Naits Eclat Tone 
+
+ Brait Xia Eist ?
+
+ Nouche Erlim end Brait imm Shiita Ween 
+
+ Mill Orfag 
+ Lishe Lans imm Lootaria end Ignai Aihel 
+
+ Kolto o Meele Fiola ou Finz Ween 
+ Mii Nai Dist Crai Foul 
+
+ Leste Yiep Sween 
 
 
 
