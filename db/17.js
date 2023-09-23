b0a8387e57dad9baead9e70dad4d4e45e3429395
kyuhubb <strylixeien@gmail.com>
2023-09-23 18:03:59 +0200

    added alician for 'kill my fortune'

--- db/17.js
+++ db/17.js
@@ -1,7 +1,148 @@
 text += `§017
 
 
+ Leste Irdliq Phina Frait , Lend Crai Awlst ?
 
+ Whiil a Giidu Rova iy Edil Ani Fiem tie Clooshela 
+
+ Leste Irdliq Phina Frait , Lend Crai Awlst ?
+
+ Poutie Xia Ran Clooshe a Toubi , Crai Vell Qleea osa Miz Volp 
+
+ Leife Leife 
+ Mii Skyar Histipri 
+
+ Herz ou Amejdimer a Heridiji Quop 
+
+ Oglejmea Graid qls end Mii Ranya Doe folme Ark Sphiep 
+
+ Xianya iy Evila Aola 
+
+ Queit Mii ol Peil a Dist Ani Lili Holstia imm Brai ou Twiik Shelista 
+
+ Ani Mols , Elaind Ani Mols 
+
+ Phina Hols a Skeem Irdliq , Gout ...
+
+ Tiisa Lef Cloud Dilem Mejkl Ekphinols 
+
+ Dia Cloud Kulu Nai Imiyl Bis 
+
+ Meidiep Unharla Ani Plousa Mols 
+
+ Wimb Phina Misiik Esp ou Xia Sole ieq Aprout 
+
+ Leste Irdliq Phina Frait , Lend Crai Awlst ?
+
+ Whiil a Giidu Rova iy Edil Ani Fiem tie Clooshela 
+
+ Leste Irdliq Phina Frait , Lend Crai Awlst ?
+
+ Poutie Xia Ran Clooshe a Toubi , iy Crai ou Volp qls ...? 
+
+ Laila Mout Saut , Baly Yutul qls 
+
+ Kulu Nai Swormin , Sar Baly Sar Olaio  
+
+ Elie Shiita Taz , Lizem Termz Karna 
+
+ Gout Ticca Mols , Lishe Heimia Crai 
+
+
+
+Judgment is about to begin Are you ready?
+
+One black and one white robe in the closet
+
+Judgment is about to begin Are you ready?
+
+After you blink those eyes, you will be one of the ghosts
+
+Hollow Hollow Trace the history
+
+The mind of fetus and The galaxy
+
+All those abominable memories I look down from the celestial sphere
+
+I think they are adorable now
+
+But there’s one wish that I’ve left behind in that world
+
+There’s only one It’s the only one
+
+I’d like to start the Judgment, but…
+
+You seem to have a great deal of regret
+
+Then you can’t go through here
+
+I’ll give you just one chance
+
+But I’ll keep a portion of that soul as a guarantee
+
+Judgment is about to begin Are you ready?
+
+One black and one white robe in the closet
+
+Judgment is about to begin Are you ready?
+
+After you blink those eyes, you will be…?
+
+Charming voice of birds Days with no big deal
+
+Can’t even fly in the sky, As usual, As before
+
+I’m going to turn the clock again until it’s time for the end
+
+But I’ll join you for a little while longer
+
+
+
+さぁ　審判が始まります　準備はできましたか？
+
+クローゼットには白と黒のローブが1枚ずつ
+
+さぁ　審判が始まります　準備はできましたか？
+
+その目を閉じて開けたなら　君もお化けになってしまいます
+
+うつろ　うつろ　歴史をたどる
+
+胎児の心と　銀河系
+
+天球から見下ろす　忌まわしい記憶たちも
+
+今では愛おしいと思うんだ
+
+けど　1つだけ　あの世界に　置き忘れてきた願いがある
+
+たった1つ　1つだけ
+
+審判を始めたいところですが…
+
+あなたは大きな未練をお持ちのようだ
+
+それではここを通ることはできません
+
+一度だけチャンスを与えましょう
+
+ただし　保障としてその魂の一部を預かります
+
+さあ　審判が始まります　準備はできましたか？
+
+クローゼットには白と黒のローブが1枚ずつ
+
+さあ　審判が始まります　準備はできましたか？
+
+その目を閉じて開けたなら　君もお化けたちの・・・？
+
+鳥のなまめかしい声　なんてことのない日々
+
+空も飛べずに　いつも通り　元通り
+
+終わりの時間まで　また時計を進めるよ
+
+でも　もう少しだけ　君に付き合ってあげよう
 
 
 
