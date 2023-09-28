71c911adc4fea3dbeb93231730118a2bac729e09
kyuhubb <strylixeien@gmail.com>
2023-09-28 17:52:15 +0200

    added alician for 'Friends'

--- script/songs_data/Friend.js
+++ script/songs_data/Friend.js
@@ -1,6 +1,41 @@
 title = "Ｆｒｉｅｎｄ";
-original_lyrics = ``;
-english_lyrics = ``;
-japanese_lyrics= ``;
+original_lyrics = ` Crai Feila Bis a Well 
+ ou Drone end Dorain ah Shelista 
+ Gout Crai Bai end Foly ah 
+ Mii Awter Crai end Oulayz Vitui 
+ Oulays Zote Oulayz Minai 
+ Crai Briss Gout 
+ Gout end Gout 
+ Mii a Crai end Crai end Crai end Crai 
+ Lootaria Saray 
+
+ end Crai Toriesta 
+ Foul Mii Crai Awter Lar Bis Elay 
+ Foul Mii Foul Mii Foul Mii 
+ Crai Lar Elay Awter 
+ Foul Mii 
+
+ Olis Crai Ailes Mii 
+ ou Drone end Dorain ah Shelista 
+ Gout Crai Bai end Foly ah 
+ Mii Awter Crai end Oulayz Vitui 
+ Oulayz Zote ( a Drone Swiina ) 
+ Oulayz Minai 
+ Mill Briss Gout 
+ Gout end Gout 
+ Mii a Crai end Crai end Crai end Crai 
+ Lootaria Saray end Mii Lili ah 
+ Saray `;
+english_lyrics = `
+
+
+
+`;
+japanese_lyrics= `
+
+
+
+
+`;
 
 importJs("../script/process");
