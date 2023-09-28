fc9909c69c8b882ce9b98ac14f54b0c9a5bed5cd
kyuhubb <strylixeien@gmail.com>
2023-09-28 19:55:54 +0200

    added alician for 'endorphin'

--- db/19.js
+++ db/19.js
@@ -1,6 +1,136 @@
 text += `§019
 
 
+ Molda end ol Yiem Ait a Leimy , Ord Drone ,
+ Luan a Spai imm Hellm 
+
+ Molda end ol Yiem Ait a Leimy , Ord Drone ,
+ Luan a Spai imm Hellm 
+
+ Dooolas Evilaqq end Briss Arch Zia 
+
+ Crai Vell Ignai Yien Rait Ord Blemenes weil Bis Pazz end ol Yien Clooshe Lef Klijka 
+
+ Yiep Qleea Razzid Oltia Velsiam Coll Falke 
+
+ Elize Kulu Dist Ail Poutie Eem Wondidda Amblait 
+
+ Poetolt Slialius Eugidha Melp Elie Piisa 
+
+ Elize Tella a Belqua Ilis Fas  
+ Hiele Phim Pleesa Hellsijma 
+
+ Staple ou Rowt Ley Klar Elie Ark 
+
+ Poetklar Yien Renz il Giila Wipsam 
+
+ Liaphi Olizes end Dilem Farp Fei Nai Guit 
+
+ Zill Vell Yien Nai Wedikarti Dou 
+
+ Poetolt Zia Haundi Ziksiilait ,
+ Poetolt Zia Sween ifa Zeit 
+ Bis iy Fikalat ou Yutulsinv 
+ Lishe Dal ieq Faress Ame 
+
+ Molda end ol Yien Ait a Leimy , Ord Drone , Luan a Spai imm Hellm 
+
+« Gete Melp Saray Lqll »
+
+« Deank il Crait Rinasijma »
+
+« Ollenes ob Inay »
+
+« Hiele , Dilem Ola Filshua , Crain ?»
+
+ Sipil ol Tollm a Eala Mezart Coll es ol Verse 
+
+
+The disconnected brain is drowning in its own dreams
+
+The disconnected brain is drowning in its own dreams
+
+A whirling lizard passes over me
+
+In this room, closed up like a capsule, no one can find you anymore
+
+Such a filthy body, just break it
+
+Pain lets me forget everything
+
+Draining away the ugly blood
+
+and pouring a rainbow into it, so now,
+show me an illusion
+
+The stairs of the abyss lead to the sky
+
+Exposed to penetrating stares
+
+That somebody with a ring around the head seems to not know
+
+that we can’t be saved
+
+Be well-behaved
+Sleep on time
+My daily routines
+Always trying to be a good boy
+
+The disconnected brain is drowning in its own dreams
+
+“This is your new family”
+
+“Thank you for being born”
+
+“We had been waiting for you”
+
+“Come on, aren’t you hungry?”
+
+The milk slipped through my body, spilled out, and faded
+
+
+切り離された脳は　独り　夢に溺れゆく
+
+切り離された脳は　独り　夢に溺れゆく
+
+僕の上を通り過ぎる　渦巻きトカゲ
+
+カプセルみたいに閉ざされたこの部屋で　君はもう誰にも見つけられない
+
+こんな汚い体　さっさと壊してしまえ
+
+痛い思いをすれば　全てを忘れられるから
+
+醜い血液を　奥底まで抜き取って
+
+虹色を流し込むから
+ねえ　幻を見せて
+
+奈落の階段は　天まで続いていくよ
+
+鋭い視線に　晒されながら
+
+丸い輪っかのついた　誰かさんは知らない
+
+僕達は　救われないんだ
+
+お行儀よくして
+時間通りに眠って
+毎日のルーティーン
+良い子でいましょう
+
+切り離された脳は　独り　夢に溺れゆく
+
+「新しい家族だよ」
+
+「産まれてきてくれてありがとう」
+
+「みんな待ってたの」
+
+「ほら　お腹が空いたでしょう?」
+
+ミルクが体をすり抜け　滴り落ち　そして消えた
+
 
 
 
--- script/songs_data/Endorphin.js
+++ script/songs_data/Endorphin.js
@@ -1,19 +1,70 @@
 title = "Endorphin";
-original_lyrics = `no lyrics`;
+original_lyrics = ` Molda end ol Yiem Ait a Leimy , Ord Drone ,
+ Luan a Spai imm Hellm 
+
+ Molda end ol Yiem Ait a Leimy , Ord Drone ,
+ Luan a Spai imm Hellm 
+
+ Dooolas Evilaqq end Briss Arch Zia 
+
+ Crai Vell Ignai Yien Rait Ord Blemenes weil Bis Pazz end ol Yien Clooshe Lef Klijka 
+
+ Yiep Qleea Razzid Oltia Velsiam Coll Falke 
+
+ Elize Kulu Dist Ail Poutie Eem Wondidda Amblait 
+
+ Poetolt Slialius Eugidha Melp Elie Piisa 
+
+ Elize Tella a Belqua Ilis Fas  
+ Hiele Phim Pleesa Hellsijma 
+
+ Staple ou Rowt Ley Klar Elie Ark 
+
+ Poetklar Yien Renz il Giila Wipsam 
+
+ Liaphi Olizes end Dilem Farp Fei Nai Guit 
+
+ Zill Vell Yien Nai Wedikarti Dou 
+
+ Poetolt Zia Haundi Ziksiilait ,
+ Poetolt Zia Sween ifa Zeit 
+ Bis iy Fikalat ou Yutulsinv 
+ Lishe Dal ieq Faress Ame 
+
+ Molda end ol Yien Ait a Leimy , Ord Drone , Luan a Spai imm Hellm 
+
+« Gete Melp Saray Lqll »
+
+« Deank il Crait Rinasijma »
+
+« Ollenes ob Inay »
+
+« Hiele , Dilem Ola Filshua , Crain ?»
+
+ Sipil ol Tollm a Eala Mezart Coll es ol Verse `;
 english_lyrics = `The disconnected brain is drowning in its own dreams
+
 The disconnected brain is drowning in its own dreams
 
 A whirling lizard passes over me
+
 In this room, closed up like a capsule, no one can find you anymore
 
 Such a filthy body, just break it
+
 Pain lets me forget everything
+
 Draining away the ugly blood
-and pouring a rainbow into it, so now, show me an illusion
+
+and pouring a rainbow into it, so now,
+show me an illusion
 
 The stairs of the abyss lead to the sky
+
 Exposed to penetrating stares
+
 That somebody with a ring around the head seems to not know
+
 that we can’t be saved
 
 Be well-behaved
@@ -24,35 +75,52 @@ Always trying to be a good boy
 The disconnected brain is drowning in its own dreams
 
 “This is your new family”
+
 “Thank you for being born”
+
 “We had been waiting for you”
+
 “Come on, aren’t you hungry?”
 
 The milk slipped through my body, spilled out, and faded`;
 japanese_lyrics= `切り離された脳は　独り　夢に溺れゆく
+
 切り離された脳は　独り　夢に溺れゆく
 
 僕の上を通り過ぎる　渦巻きトカゲ
+
 カプセルみたいに閉ざされたこの部屋で　君はもう誰にも見つけられない
 
 こんな汚い体　さっさと壊してしまえ
+
 痛い思いをすれば　全てを忘れられるから
+
 醜い血液を　奥底まで抜き取って
-虹色を流し込むから　ねえ　幻を見せて
+
+虹色を流し込むから
+ねえ　幻を見せて
 
 奈落の階段は　天まで続いていくよ
+
 鋭い視線に　晒されながら
+
 丸い輪っかのついた　誰かさんは知らない
+
 僕達は　救われないんだ
 
-お行儀よくして　時間通りに眠って
-毎日のルーティーン　良い子でいましょう
+お行儀よくして
+時間通りに眠って
+毎日のルーティーン
+良い子でいましょう
 
 切り離された脳は　独り　夢に溺れゆく
 
 「新しい家族だよ」
+
 「産まれてきてくれてありがとう」
+
 「みんな待ってたの」
+
 「ほら　お腹が空いたでしょう?」
 
 ミルクが体をすり抜け　滴り落ち　そして消えた`;
