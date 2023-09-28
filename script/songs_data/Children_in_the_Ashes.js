2e8ef5cdae81470d96076f513be34202f8514a1f
kyuhubb <strylixeien@gmail.com>
2023-09-28 22:21:37 +0200

    added alician for 'Children in the Ashes'

--- script/songs_data/Children_in_the_Ashes.js
+++ script/songs_data/Children_in_the_Ashes.js
@@ -1,58 +1,151 @@
 title = "Children in the Ashes";
-original_lyrics = `no lyrics`;
+original_lyrics = ` Mii Nai Dilem Harla Bai Finz Taz Joul Elay 
+
+ Imeila Tiag Myahalke , Vell Ignai Deet 
+
+ Xia Nai Arth Amblu , Foul Vell Kulu Nai Rait
+
+ Bisoli iy Ail ou Myte Shelista 
+
+ Cloudyte Tittri iy Lef Bis , Cloudyte Herches iy Bisoli 
+
+ Meidiep Cloud Gete Sole 
+
+" Crai Nai Anisia Bai sii ,"
+
+" Hiele Lishe Klattle Haol "
+
+" Crai iy Nai Drone folme Aolu "
+
+ Leste Idial Sween Ollenes Poetolt Sworntie il Tone ou Jilga 
+
+ Tozlom Arla Piino Swiina Ail , Xia Wond Vell Fliiad 
+
+ Olzy Nai Fiela Bai , Olzy Nai Fiela Zel Note 
+
+ Zeit end ob Stica ol Eid a Skeem Yulleia 
+
+ Crai ol Uarmeil Poetolt Kueip Herz Dorain 
+
+ Wasiim Crai Poet Wasiim Crai Poet 
+
+ Elize Shelista Fur a Leimy Zill Baly 
+ Kueip Gete Shelista Ord Bis Haol Dou 
+
+ ( " Crai Nai Anisia Bai sii ,"
+ " Hiele Lishe Klattle Haol " 
+ " Crai iy Nai Drone folme Aolu " 
+ " Crai Nai Anisia Bai sii ,"
+ " Hiele Lishe Klattle Haol "
+ " Crai iy Nai Drone folme Aolu ") 
+
+ Leste Allss il Hellm ol Brait 
+
+ Ene qls end Wondilip Crai Nai Aihel Amblu 
+ Nai Aihel Bai 
+
+ Poet Nai Kllea , Poet Nai Kllea ,
+ Yiela Sween Aola Bai 
+
+ Laiz end Crai Colista Elay Klar Tikelt Xiaoli `;
 english_lyrics = `I’m done
 I’m so over it
+
 My voice never reaches, no matter how many times I scream
 
 It’s nowhere to be seen
 It’s never to be found
+
 This is my whole world
 
 Eyes like this, heart like this
+
 I’ll give you a new soul
-“We’re not lonely anymore” “Let’s hold our hands”
+
+“We’re not lonely anymore”
+
+“Let’s hold our hands”
+
 “You’re not alone from today on”
 
 Come, let’s get on a monster and put everyone to sleep
+
 And when the white breath envelops everything, your wound will heal
+
 You don’t have to be afraid anymore
 You don’t have to be afraid of anything
+
 The time which had been stopped began to proceed slowly
 
 You dropped your head with a gloomy heart
+
 Why, why?
+
 The world has always abandoned us,
 so we’re going to make a new world with our own hands
 
+“We’re not lonely anymore”
+“Let’s hold our hands”
+“You’re not alone from today on”
+“We’re not lonely anymore”
+“Let’s hold our hands”
+“You’re not alone from today on”
+
 Now, the dream eyelids are open
+
 No one in this world will hurt you
 They’re all gone
+
 Don’t cry, don’t cry
 Sleep tight, sweetheart
+
 The future you want is just around the corner`;
 japanese_lyrics= `もういいよ　もうだめなんだ
+
 何回叫んでも　届くことはない
-どこにもいない　きっと見つからない　
+
+どこにもいない　きっと見つからない
+
 ここが　私の世界のすべて
 
 あなたの瞳はこうで心臓はここ
-新しい魂を授けよう　
-‟もう寂しくないね　ほら手を繋ごう”
+
+新しい魂を授けよう
+
+‟もう寂しくないね”
+
+‟ほら手を繋ごう”
+
 ‟今日から　君は独りじゃない”
 
 さぁ　怪獣の背中へ飛び乗って　みんな眠らせてしまおう
+
 白い吐息が全てを包むとき　その傷は癒えるだろう
+
 もう恐れなくていい　何も恐れなくていい
+
 止まっていた時間が　ゆっくりと動き出した
 
 君はうつむいた　こころを　真っ暗にして
+
 どうして　どうして
+
 世界はいつも　僕らを見放すから
 この手で　新しい世界を作るんだ
 
+‟もう寂しくないね”
+‟ほら手を繋ごう”
+‟今日から　君は独りじゃない”
+‟もう寂しくないね”
+‟ほら手を繋ごう”
+‟今日から　君は独りじゃない”
+
 さあ　夢の瞼が開いたよ
+
 君を傷つけるものはどこにもいない 　もういない
+
 泣かないで　泣かないで　今はもうお眠りなさい
+
 君が望む未来は　すぐそこに`;
 
 importJs("../script/process");
