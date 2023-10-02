2dfc643a3cac88691a5044a2c34606a89c8bb4f2
kyuhubb <strylixeien@gmail.com>
2023-10-02 17:12:53 +0200

    added alician for 'Urxbxrxs' + added words in dictionary

--- db/23.js
+++ db/23.js
@@ -1,7 +1,194 @@
 text += `§023
 
 
+ Ullem ou Epkejla Lef Dizzlet ,
+ Bipeidit Garei ,
+ Wendit Lusia Entie Arch Synapse ,
+ il Eiti Minelu 
 
+ Sikid Pillusi Olipha en Selanya Helfigi ou Fleekijt 
+
+－ Blemyte Holtica iy Xia 
+
+ Jay Saut end Mii Iar folme Loqcka Oli 
+
+ Mineldari Quop end Acary Seeda ,
+ Herches ou Minelbli ,
+
+ Poutie Skima Fei end Kremsa Nache Eidarit 
+ iy Mazy ou Guidial Spay end Qtua Loosya Selti 
+
+ Heivi Ijti Awdrom Shelimlait Iequim 
+ Lef Folsiagol end ol Yien Esc Helgiek 
+
+ Seekle Phida Rinasia lid Shelim Nouche 
+
+ Graid qls ou Loqcka Yutul Avis folme Eidarit 
+
+ Poetolt Millarm Heizid Ene Salah imm Aishellm ,
+
+ ol Sigik Myte Ran folme Tvulei end Colista Etusyatt 
+
+ Xia Mariju Imeila Mii Leidiq Kerla ,
+ Poetolt Xia Squa Fimoa Fouzanoalait 
+
+ Hiele Blem iy Crai end Aldaltia Myte Fiakli 
+
+ Ullem ou Epkejla Lef Dizzlet ,
+ Bipeidit Garei ,
+ Wendit Lusia Entie Arch Synapse ,
+ il Eiti Minelu 
+
+ Sikid Pillusi Olipha en Selanya Helfigi ou Fleekijt 
+
+－ Blemyte Holtica iy Xia 
+
+ Jay Saut end Mii Iar folme Loqcka Oli 
+
+ Mineldari Quop end Acary Seeda ,
+ Herches ou Minelbli ,
+
+ Poutie Skima Fei end Kremsa Nache 
+ Eidarit 
+ iy Mazy ou Guidial Spay end Qtua Loosya Selti 
+
+ Heivi Ijti Awdrom Shelimlait Iequim
+ Lef Folsiagol end ol Yien Esc Helgiek 
+
+ Xia iy Ofelnti end Apeilf Yullk tie Fete Miis Vell Yien Womphil 
+
+ Poetklar Myte Herz Yien Torjest ,
+ Mii Fiesmes Pillusi Ail Ienadip 
+
+ Imm Brai ou Bis Wendiak Fei end 
+ ol Yien Dikteb Ord Olizenote 
+
+ Arte Edlijtu ,
+ Arte Heiss ,
+ Porsa Tella Aka Prii end Vell Jac a Verse 
+
+
+Wriggling neurons deep in my splitting head
+Swimming lights ride the synapse to the next star
+
+Consciousness has lost its way, wandering between dimensions
+
+-Whose sins are those?
+
+I hear a sad voice from afar
+
+Planetary systems guided by revelation,
+the stellar heart
+
+Go through the circle of elementary particles at play
+We find a labyrinth of mirrors that mocks even every meaning
+
+I silently bite down the loud order
+Like a beast with its fangs pulled out
+
+In the silence of the night, a slight distortion is born
+
+Peeking through it are the memories of the past
+
+Gazing absent-mindedly at an innocent figure
+
+I turned my eyes away from the figure begging for help
+
+No matter how much I run, you keep chasing me, unnaturally hiding your face
+
+Hey, who are you to strangle me?
+
+Wriggling neurons deep in my splitting head
+Swimming lights ride the synapse to the next star
+
+Consciousness has lost its way, wandering between dimensions
+
+-Whose sins are those?
+
+I hear a sad voice from afar
+
+Planetary systems guided by revelation, the stellar heart
+
+Go through the circle of elementary particles at play
+We find a labyrinth of mirrors that mocks even every meaning
+
+I silently bite down the loud order
+Like a beast with its fangs pulled out
+
+Like plankton swaying in the fate of being swallowed
+
+Overwhelmed and at loss
+
+In this huge circle created by power
+
+Sorrowing,
+Smiling,
+Just shedding drops of water that only dry up and disappear
+
+
+割れるような頭の奥
+蠢くニューロン
+泳ぐ光はシナプスに乗り
+次の星へ
+
+意識は行き場を失くし　次元の狭間を彷徨うのだ
+
+－その罪は誰のもの
+
+遠くから聴こえる　悲しい声
+
+黙示が導く惑星系
+恒星の心臓
+
+素粒子の戯れる輪を潜れば
+そこは　意味すら嘲る　合わせ鏡の迷路
+
+高らかな命令を　静かに噛みしめる
+牙を抜かれた　獣みたいに
+
+夜の静けさに　僅かな歪みが生まれる
+
+そこから覗くのは　いつかの記憶たち
+
+あどけない人影を　無心に見つめては
+
+助けを乞う姿から　目を逸らした
+
+どれだけ逃げても追いかけてくる
+不自然にその顔を隠して
+
+ねえ　この首を絞める　君は誰
+
+割れるような頭の奥
+蠢くニューロン
+泳ぐ光はシナプスに乗り
+次の星へ
+
+意識は行き場を失くし　次元の狭間を彷徨うのだ
+
+－その罪は誰のもの
+
+遠くから聴こえる　悲しい声
+
+黙示が導く惑星系
+恒星の心臓
+
+素粒子の戯れる輪を潜れば
+そこは　意味すら嘲る　合わせ鏡の迷路
+
+高らかな命令を　静かに噛みしめる
+牙を抜かれた　獣みたいに
+
+それは　呑み込まれる運命の中　揺蕩うプランクトン
+
+打ちひしがれながら
+成す術もない
+
+何かによって構築された　この巨大な輪の中で
+
+憂いたり
+微笑んだり
+乾いて消えるだけの水滴を流すだけ
 
 
 
