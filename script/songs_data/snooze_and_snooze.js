a9c8908ef257fa741e33504ee0023457e2569201
kyuhubb <strylixeien@gmail.com>
2024-06-10 15:16:43 +0200

    update lyrics snooze & snooze

--- script/songs_data/snooze_and_snooze.js
+++ script/songs_data/snooze_and_snooze.js
@@ -1,44 +1,166 @@
 title = "Sno(_ _)oze & Sn(o_o)ze";
 original_lyrics = ` Aolu Story end Bale ?
+
  a Story Olis Flute 
+
  Aolu Story end Bale ?
+
  a Story Olis Flute 
+
  Minsty ah Story end Story ah Bale 
+
  Staps Termz end Ran Hise 
+
  Hamil Slay !
  Hamil Slay !
+
  Selaf .
 
  ah Aolu Story end Bale ?
+
  a Story Olis Flute 
+
  Aolu Story end Bale ?
+
  a Story Olis Flute 
+
  Minsty ah Story end Story ah Bale 
+
  Staps Termz end Ran Hise 
+
  Hamil Slay !
  Hamil Slay !
+
  Selaf .
 
  Fiiani end Swiina Mail 
+
  Umm .. Mii Bell Gout Mii Nai Bell 
+
  Lans Sail , Seiren Minelia 
+
  Umm .. Mii Bell end ... 
+
  Ahh Mii Faress end Mii Faress ?
+
  ah Mii Wiina end 
+
  Karna Mii Nache Sar Crai 
+
  Oul Quest 
+
  a Quest 
+
  Quest 
- Quest ..`;
+
+ Quest ...`;
 english_lyrics = `
 
 
 
-`;
-japanese_lyrics= `
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 `;
+japanese_lyrics= `今天的故事是什么样？
+
+是个欢快无比的故事
+
+今天的故事是什么样？
+
+是个欢快无比的故事
+
+明天的故事会是什么样的故事
+
+拍拍时钟 揉揉眼睛
+
+早上好！
+早上好！
+
+（现实）世界。
+
+今天的故事是什么样？
+
+是个欢快无比的故事
+
+今天的故事是什么样？
+
+是个欢快无比的故事
+
+明天的故事会是什么样的故事
+
+拍拍时钟 揉揉眼睛
+
+早上好！
+早上好！
+
+（现实）世界。
+
+做个深呼吸 换上衣服
+
+唔…我似乎记得…又似乎不记得…
+
+起舞的太阳，歌唱的月儿
+
+唔…我似乎见过…
+
+哎呀，无所谓吧？无所谓了吧？
+
+好想睡哦
+
+不过，下次再一起玩吧
+
+再度踏上宏大的冒险
+
+踏上冒险
+
+冒险
+
+冒险…`;
 
 importJs("../script/process");
