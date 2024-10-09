1c0a86996995ced81e0f6121cc4b14608f48bf57
kyuhubb <strylixeien@gmail.com>
2024-10-09 05:16:37 +0200

    wanderers : japanese + english lyrics & updated dictionary

--- db/44.js
+++ db/44.js
@@ -76,6 +76,154 @@ Kampiela joul Razzle, Kulu Nai Falisiam Bai
 
 
 
+I'm an amber star, a world without suffering
+
+We who have no wings will never get there
+
+Melt in the noisy night and sink to the bottom
+
+The door to that star will never open
+
+I heard the sound of water dripping
+
+Rusty smell, Rusty smell
+
+I heard the sound of water dripping
+
+Rusty smell, rusty smell
+
+In a nightmare
+
+Go on, Like caressing the dull sky
+
+Go on, Like crawling to the edge of the planet
+
+Holding darkness in our arms, Go on, Go on
+
+In a nightmare
+
+Live, Like running over them one by open
+
+Live, Like washing away our remorse
+
+The compass is broken. There's no turning back
+
+It's an amber star, a world without suffering
+
+We who have no wings will never get There's
+
+Metl in the noisy night and sink to the bottom
+
+The door to that star will never open
+
+When our heart is covered with grief
+
+The reason for the world,
+
+a little light wakes up its eyes
+
+Let's recite a spell with a sooty body to blacken our preodained destiny
+
+I heard the sound of water dripping
+
+Rusty smell, rusty smell
+
+I heard the sound of water dripping
+
+Rusty smell, rusty smell
+
+In a nightmare
+
+Go on, Like caressing the dull sky
+
+Go on, Like crawling to the edge of the planet
+
+Holding darkness in our arms, Go on, Go one
+
+In a nightmare
+
+Live, Like running over them one by open
+
+Live, Like washing away our remorse
+
+The compass is broken. There's no turning back
+
+
+
+それは　琥珀色の星　苦しみのない世界
+
+翼のない私達には　きっと辿り着けないね
+
+諍いの夜に溶けて　底へ沈む
+
+その星への扉は　開かれることはない
+
+滴る水の音がする
+
+錆びた匂い　錆びた匂い
+
+滴る水の音がする
+
+錆びた匂い　錆びた匂い
+
+悪夢の中
+
+進め　鈍色の空を撫でるように
+
+進め　惑星の果てを這うように
+
+暗闇を抱きかかえながら　進め　進め
+
+悪夢の中
+
+生きるのだ　ひとつひとつ轢殺するように
+
+生きるのだ　自責の念を洗い流すように
+
+羅針盤は壊れてしまった　もう引き返すことはできない
+
+それは　琥珀色の星　苦しみのない世界
+
+翼のない私達には　きっと辿り着けないね
+
+諍いの夜に溶けて　底へ沈む
+
+その星への扉は　開かれることはない
+
+心が悲しみに覆われるとき　
+
+世界の理は　小さな灯は　その目を覚ます　
+
+煤けた体で　呪文を唱えましょう
+
+決められた運命を　黒く塗り潰すために
+
+滴る水の音がする
+
+錆びた匂い　錆びた匂い
+
+滴る水の音がする
+
+錆びた匂い　錆びた匂い
+
+悪夢の中
+
+進め　鈍色の空を撫でるように
+
+進め　惑星の果てを這うように
+
+暗闇を抱きかかえながら　進め　進め
+
+悪夢の中
+
+生きるのだ　ひとつひとつ轢殺するように
+
+生きるのだ　自責の念を洗い流すように
+
+羅針盤は壊れてしまった　もう引き返すことはできない
+
+
+
 `;
 
 bookloaded++;
--- script/songs_data/Wanderers.js
+++ script/songs_data/Wanderers.js
@@ -1,71 +1,218 @@
 title = "Wanderers";
 original_lyrics = ` Xia iy Enphil Minelu , Shelista end Dim Nai Aihel 
+
  Elzanoa Mill Vell Ignai Flojrie sii 
+
  Poetolt Weliim il Nouche ou Folqui , Wely il Kolto 
+
  Eist il Xia Minelu Ignai Yien Brait 
 
  Iar Pray ou Aka end Tollti 
+
  Sjeidmea Ijsya , Sjeidmea Ijsya 
+
  Iar Pray ou Aka end Tollti 
+
  Sjeidmea Ijsya Sjeidmea Ijsya 
 
  Tie Vinshellm 
+
  Lef Rindou Vleitiad Sarga , Yiep Liz 
+
  Lef Fea Ween ou Mineldari , Yiep Liz 
+
  Poetklar Heipe a Dilem Doraid , Yiep Liz , Yiep Liz 
+
  Tie Vinshellm 
+
  Lef Gllind Plousa end Plousa , Ditka Ellif 
+
  Lef Kllea orilim Holtit , Ditka Ellif 
+
  Kampiela joul Razzle , Kulu Nai Falisiam Bai 
 
  Xia iy Enphil Minelu , Shelista end Dim Nai Aihel 
+
  Elzanoa Mill Vell Ignai Flojrie sii 
+
  Poetolt Weliim il Nouche ou Folqui , Wely il Kolto 
+
  Eist iy Xia Minelu Ignai Yien Brait 
 
  Tozlom Herz Yien Dybytt Ord jayt 
+
  Arliqlowt ou Shelista , A Krey Tim Vell Amel 
+
  Lishe Nolm Disverry ifa Iladmea Coll 
+
  Arsiam Hiip a Tigalm Fete end Yien Siiqlea Ord Giidu 
 
  Iar Pray ou Aia end Tollti 
+
  Sjeidmea Ijsya , Sjeidmea Ijsya 
+
  Iar Pray ou Aia end Tollti 
+
  Sjeidmea Ijsya , Sjeidmea Ijsya 
 
  Tie Vinshellm 
+
  Lef Rindou Vleitiad Sarga , Yiep Liz 
+
  Lef Fea Ween ou Mineldari , Yiep Liz 
+
  Poetklar Heipe a Dilem Doraid , Yiep Liz , Yiep Liz 
+
  Tie Vinshellm 
+
  Lef Gllind Plousa end Plousa , Ditka Ellif 
+
  Lef Kllea orilim Holtit , Ditka Ellif 
+
  Kampiela joul Razzle , Kulu Nai Falisiam Bai `;
-english_lyrics = `
+english_lyrics = `I'm an amber star, a world without suffering
 
+We who have no wings will never get there
 
+Melt in the noisy night and sink to the bottom
 
+The door to that star will never open
 
+I heard the sound of water dripping
 
+Rusty smell, Rusty smell
 
+I heard the sound of water dripping
 
+Rusty smell, rusty smell
 
+In a nightmare
 
+Go on, Like caressing the dull sky
 
+Go on, Like crawling to the edge of the planet
 
+Holding darkness in our arms, Go on, Go on
+
+In a nightmare
+
+Live, Like running over them one by open
+
+Live, Like washing away our remorse
+
+The compass is broken. There's no turning back
+
+It's an amber star, a world without suffering
+
+We who have no wings will never get There's
+
+Metl in the noisy night and sink to the bottom
+
+The door to that star will never open
+
+When our heart is covered with grief
+
+The reason for the world,
+
+a little light wakes up its eyes
+
+Let's recite a spell with a sooty body to blacken our preodained destiny
+
+I heard the sound of water dripping
+
+Rusty smell, rusty smell
+
+I heard the sound of water dripping
+
+Rusty smell, rusty smell
+
+In a nightmare
+
+Go on, Like caressing the dull sky
+
+Go on, Like crawling to the edge of the planet
+
+Holding darkness in our arms, Go on, Go one
+
+In a nightmare
+
+Live, Like running over them one by open
+
+Live, Like washing away our remorse
+
+The compass is broken. There's no turning back
 `;
-japanese_lyrics= `
+japanese_lyrics= `それは　琥珀色の星　苦しみのない世界
 
+翼のない私達には　きっと辿り着けないね
 
+諍いの夜に溶けて　底へ沈む
 
+その星への扉は　開かれることはない
 
+滴る水の音がする
 
+錆びた匂い　錆びた匂い
 
+滴る水の音がする
 
+錆びた匂い　錆びた匂い
 
+悪夢の中
 
+進め　鈍色の空を撫でるように
 
+進め　惑星の果てを這うように
 
+暗闇を抱きかかえながら　進め　進め
+
+悪夢の中
+
+生きるのだ　ひとつひとつ轢殺するように
+
+生きるのだ　自責の念を洗い流すように
+
+羅針盤は壊れてしまった　もう引き返すことはできない
+
+それは　琥珀色の星　苦しみのない世界
+
+翼のない私達には　きっと辿り着けないね
+
+諍いの夜に溶けて　底へ沈む
+
+その星への扉は　開かれることはない
+
+心が悲しみに覆われるとき　
+
+世界の理は　小さな灯は　その目を覚ます　
+
+煤けた体で　呪文を唱えましょう
+
+決められた運命を　黒く塗り潰すために
+
+滴る水の音がする
+
+錆びた匂い　錆びた匂い
+
+滴る水の音がする
+
+錆びた匂い　錆びた匂い
+
+悪夢の中
+
+進め　鈍色の空を撫でるように
+
+進め　惑星の果てを這うように
+
+暗闇を抱きかかえながら　進め　進め
+
+悪夢の中
+
+生きるのだ　ひとつひとつ轢殺するように
+
+生きるのだ　自責の念を洗い流すように
+
+羅針盤は壊れてしまった　もう引き返すことはできない
 `;
 
 importJs("../script/process");
